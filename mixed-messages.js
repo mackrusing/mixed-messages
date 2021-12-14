e224f25bfe073dbc1a49d21f869a52caf79bf6be
Mack Rusing <mackrusing@gmail.com>
2021-12-13 23:16:39 -0700

    Add early planning to js file

--- mixed-messages.js
+++ mixed-messages.js
@@ -1,5 +1,45 @@
-// Mixed Messages
 // Mack Rusing
+// Mixed Messages
 // 2021-12-13
 
+// first names (masculine)
+const masculineNames = []
 
+// first names (feminine)
+const feminineNames = []
+
+// first names (neutral)
+const neutralNames = []
+
+// last names
+const lastNames = []
+
+// titles
+const titles = []
+
+// town names pt 1
+const townName1 = []
+
+// town names pt 2
+const townName2 = []
+
+// alignments
+const alignments = []
+
+// life styles
+const lifeStyles = []
+
+
+
+/* 
+ideas: 
+- create object of character
+- calculate chance of selection
+
+- name (random name gen)
+- parents (random name gen)
+- birthplace (random town gen)
+- current age 
+- alignment
+- life style
+*/ 
