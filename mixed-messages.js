f84531beb98a58a7babb4f67cf58af529b7d1dcc
Mack Rusing <mackrusing@gmail.com>
2021-12-14 11:27:41 -0700

    Update js

--- mixed-messages.js
+++ mixed-messages.js
@@ -2,46 +2,54 @@
 // Mixed Messages
 // 2021-12-13
 
-// first names (masculine)
-const masculineNames = []
-
-// first names (feminine)
-const feminineNames = []
-
-// first names (neutral)
-const neutralNames = []
-
-// all names
-const allNames = [...masculineNames, ...feminineNames, ...neutralNames]
-
-// last names
-const lastNames = []
-
-// titles
-const titles = []
-
-// town names pt 1
-const townName1 = []
-
-// town names pt 2
-const townName2 = []
-
-// alignments
-const alignments = []
-
-// life styles
-const lifeStyles = []
+// variables for random character gen
+const masculineNames = [
+  'Joe'
+];
+const feminineNames = [
+  'Jean'
+];
+const neutralNames = [
+  'Jamie'
+];
+const allNames = [
+  ...masculineNames, 
+  ...feminineNames, 
+  ...neutralNames
+];
+const lastNames = [
+  'Luthorn'
+];
+const titles = [
+  'Destroyer of Worlds'
+];
+const townName1 = [
+  'North'
+];
+const townName2 = [
+  'shire'
+];
+const races = [
+  'human'
+];
+const alignments = [
+  'neutral good'
+];
+const lifeStyles = [
+  'poor'
+];
 
 // character object factory
-function characterFactory(name, parent1, parent2, birthplace, age, alignment, lifeStyle) {
+function characterFactory(name, parent1, parent2, birthplace, age, race, alignment, lifeStyle) {
   return {
     name,
     parent1,
     parent2,
     birthplace,
     age,
+    race,
     alignment,
-    lifeStyle,
+    lifeStyle
   }
 }
 
@@ -49,7 +57,16 @@ function characterFactory(name, parent1, parent2, birthplace, age, alignment, li
 function createRandomCharacter() {
   let lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
   let fullName = `${allNames[Math.floor(Math.random() * allNames.length)]} ${lastName}`;
+  let parent1Name = `${allNames[Math.floor(Math.random() * allNames.length)]} ${lastName}`;
+  let parent2Name = `${allNames[Math.floor(Math.random() * allNames.length)]} ${lastName}`;
+  let birthplace = townName1 + townName2;
+  let age = Math.floor(Math.random() * 35) + 16; // random age from 16-50
+  let race = races[Math.floor(Math.random() * races.length)];
+  let alignment = alignments[Math.floor(Math.random() * alignments.length)];
+  let lifeStyle = lifeStyles[Math.floor(Math.random() * lifeStyles.length)];
+  return characterFactory(fullName, parent1Name, parent2Name, birthplace, age, race, alignment, lifeStyle);
 }
+
 
 
 /* 
@@ -57,11 +74,15 @@ ideas:
 - create object of character
 - calculate chance of selection
 - old / young / etc in message based on age
+- check for identical names
+- put data in an object, add getters and setters
 
+- race
 - name (random name gen)
 - parents (random name gen)
 - birthplace (random town gen)
 - current age 
 - alignment
 - life style
+- title
 */ 
