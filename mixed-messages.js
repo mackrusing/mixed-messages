164965f799120e867782a06415847bb9fa02a395
Mack Rusing <mackrusing@gmail.com>
2021-12-15 15:37:07 -0700

    Update info method of character object

--- mixed-messages.js
+++ mixed-messages.js
@@ -2,11 +2,38 @@
 // Mixed Messages
 // 2021-12-13
 
+// import players handbook data
+const dataPool = require('./players-handbook.json');
+
+// capitalize function
+function capitalize(input) {
+  return input[0].toUpperCase() + input.slice(1);
+}
+
+// pronouns
+const pronouns = {
+  masculine: {
+    subjective: 'he',
+    objective: 'him',
+    possessive: 'his'
+  },
+  feminine: {
+    subjective: 'she',
+    objective: 'her',
+    possessive: 'hers'
+  },
+  neutral: {
+    subjective: 'they',
+    objective: 'them',
+    possessive: 'theirs'
+  }
+}
+
 // character object factory
 function characterFactory(name, pronouns, race, chClass, background, skin, eyes, hair, height, weight, age, alignment, personalityTraits, ideals, bonds, flaws) {
   return {
-    name,
-    pronouns,
+    name, // first and last
+    pronouns, // masculine (he/him), feminine (she/her), or neutral (they/them)
     race,
     chClass,
     background,
@@ -14,8 +41,8 @@ function characterFactory(name, pronouns, race, chClass, background, skin, eyes,
       skin,
       eyes,
       hair,
-      height,
-      weight,
+      height, // in cm
+      weight, // in kg
       age
     },
     characteristics: {
@@ -26,47 +53,42 @@ function characterFactory(name, pronouns, race, chClass, background, skin, eyes,
       flaws
     },
     info() {
-
+      // age group
+      let ageGroup = 'aspiring'
+      if (this.appearance.age > 25) {
+        ageGroup = 'young';
+      } else if (this.appearance.age > 00) {
+        ageGroup = 'seasoned';
+      } else {
+        ageGroup = 'vetran';
+      }
+      console.log(
+        `${this.name} is a ${ageGroup} ${this.race} ${this.chClass}.`
+      );
     }
   }
 }
 
 // create a random character
 function createRandomCharacter() {
-  let familyName = familyNames[Math.floor(Math.random() * familyNames.length)];
-  let fullName = `${allNames[Math.floor(Math.random() * allNames.length)]} ${familyName}`;
-  let pronons = pronouns[Math.floor(Math.random() * pronouns.length)];
-  let parent1Name = `${allNames[Math.floor(Math.random() * allNames.length)]} ${familyName}`;
-  let parent2Name = `${allNames[Math.floor(Math.random() * allNames.length)]} ${familyName}`;
-  let birthplace = townName1 + townName2;
-  let age = Math.floor(Math.random() * 35) + 16; // random age from 16-50
-  let race = races[Math.floor(Math.random() * races.length)];
-  let alignment = alignments[Math.floor(Math.random() * alignments.length)];
-  let lifeStyle = lifeStyles[Math.floor(Math.random() * lifeStyles.length)];
 }
 
-let testData = require('./players-handbook.json');
-console.log(testData.races.dwarf.names.first)
+// test cases
+const testCharacter = characterFactory('Quinn Hightopple', 'masculine', 'halfling', 'rogue', 'criminal', 'fair', 'brown', 'brown', 89, 17, 21, 'neutral', "Nice set of teeth you got there. Be a shame if something bad happened to 'em.", "No challenge is too big to overcome. As Papa Bartho always says, the bigger they are, the harder they fall", "Nothing's more important than friendship. That's why I'll never leave a friend behind.", "I can't resist punching tall folk in the groin. I call it the Halfling Hello.");
+
+console.log(testCharacter);
+
+testCharacter.info();
+
 
 /* 
 ideas: 
-- create object of character
-- calculate chance of selection
 - old / young / etc in message based on age
-- check for identical names
 - put data in an object, add getters and setters
 - randomization limits based on race
-
-- race
-- class
-- name (random name gen)
-- parents (random name gen)
-- birthplace (random town gen)
-- town size
-- current age 
-- alignment
-- life style
-- title
+- create random parents for random character
+- create random birthplace town for random character
+  - town size
 
 NAME is a(n) AGE GROUP RACE CLASS at the age of AGE. FIRST NAME was raised by 
 PARENT 1 FIRST NAME and PARENT 2 FIRST NAME LAST NAME in the SIZE 
@@ -74,24 +96,4 @@ settlement/town/city of BIRTHPLACE.
 
 Quinn Hightopple is a young Halfling Rouge. Quinn was raised by Bartho and 
 Qelline Hightopple in the small town of Northshire. 
-
-- name (first + last)
-- pronouns
-- race
-- class
-- background
-- appearance
-  - skin
-  - eyes
-  - hair
-  - height
-  - weight
-  - age
-- characteristics
-  - alignments
-  - personality traits
-  - ideals
-  - bonds
-  - flaws
-- 
 */ 
